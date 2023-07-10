fd2c5151ad445357ec15b5c7a815b0dfb5a713ac
gemii0606 <gemii0606@gmail.com>
2023-07-10 10:06:14 +0800

    Update picture_update.js

--- api/1.0/users/picture_update.js
+++ api/1.0/users/picture_update.js
@@ -27,7 +27,7 @@ const pool = require('../utils/mysql');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/api/1.0/public'); 
+        cb(null, '../public'); 
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname.split('.')[0]  + '_' + Date.now() + '.jpg'); 
