18d76f312105fec29f4b8c4759f14c2d242c95d1
gemii0606 <gemii0606@gmail.com>
2023-07-10 16:25:21 +0800

    Update server.js

--- api/1.0/server.js
+++ api/1.0/server.js
@@ -10,8 +10,8 @@ app.use('/api/1.0/users', usersRoute);
 const friendsRoute = require('./friends');
 app.use('/api/1.0/friends', friendsRoute);
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/api/1.0/friends/:user_id/request', (req, res) => {
+  res.send('Hello, friends!');
 });
 
 app.get('/api/1.0/users/signin', (req, res) => {
