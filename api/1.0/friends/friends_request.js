77b08f583e2ec34d9d6b480547074085138c2868
gemii0606 <gemii0606@gmail.com>
2023-07-10 22:42:45 +0800

    Update friends_request.js

--- api/1.0/friends/friends_request.js
+++ api/1.0/friends/friends_request.js
@@ -7,9 +7,9 @@ const { checkAuthorization } = require('../utils/function');
 // set the connection with mysql server
 const pool = require('../utils/mysql');
 
-
-router.get('/', checkAuthorization, async (req, res) => {
-    res.send('request');
+console.log('requset1');
+router.get('/',  async (req, res) => {
+    res.send('request2');
     // const decodedToken = req.decodedToken;
     // console.log(decodedToken);
     // console.log(req.params.user_id);
