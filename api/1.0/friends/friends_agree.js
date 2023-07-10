8c153d1c0a2fa0e7e63694e648eb27ab6bfbfd92
gemii0606 <gemii0606@gmail.com>
2023-07-10 22:22:29 +0800

    1

---
+++ api/1.0/friends/friends_agree.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', checkAuthorization, async (req, res) => {
+    res.send('agree');
+});
--- api/1.0/friends/friends_request.js
+++ api/1.0/friends/friends_request.js
@@ -8,36 +8,37 @@ const { checkAuthorization } = require('../utils/function');
 const pool = require('../utils/mysql');
 
 
-router.post('/', checkAuthorization, async (req, res) => {
-    const decodedToken = req.decodedToken;
-    console.log(decodedToken);
-    console.log(req.params.user_id);
-    const user_id = decodedToken.id;
-    const request_id = req.params.user_id;
+router.get('/', checkAuthorization, async (req, res) => {
+    res.send('request');
+    // const decodedToken = req.decodedToken;
+    // console.log(decodedToken);
+    // console.log(req.params.user_id);
+    // const user_id = decodedToken.id;
+    // const request_id = req.params.user_id;
 
-    const [selectUsers] = await pool.query('SELECT * FROM users WHERE id = ?', [request_id]);
-    if (selectUsers.length === 0) {
-        return res.status(400).json({error: 'User does not exist.'});
-    }
+    // const [selectUsers] = await pool.query('SELECT * FROM users WHERE id = ?', [request_id]);
+    // if (selectUsers.length === 0) {
+    //     return res.status(400).json({error: 'User does not exist.'});
+    // }
 
 
-    const [AtoB] = await pool.query('SELECT * FROM users WHERE user_id = ? AND friend_id = ?', [user_id, request_id]);
-    if (AtoB !== 0) {
-        return res.status(400).json({error: 'The request has already sent'});
-    }
+    // const [AtoB] = await pool.query('SELECT * FROM users WHERE user_id = ? AND friend_id = ?', [user_id, request_id]);
+    // if (AtoB !== 0) {
+    //     return res.status(400).json({error: 'The request has already sent'});
+    // }
 
-    const [BtoA] = await pool.query('SELECT * FROM users WHERE friend_id = ? AND user_id = ?', [user_id, request_id]);
-    if (BtoA[0].status === 'pending') {
-        return res.status(400).json({error: 'He/She is waiting for your acceptance.'});
-    }
-    if (BtoA[0].status === 'accepted') {
-        return res.status(400).json({error: 'You are friends already.'});
-    }
+    // const [BtoA] = await pool.query('SELECT * FROM users WHERE friend_id = ? AND user_id = ?', [user_id, request_id]);
+    // if (BtoA[0].status === 'pending') {
+    //     return res.status(400).json({error: 'He/She is waiting for your acceptance.'});
+    // }
+    // if (BtoA[0].status === 'accepted') {
+    //     return res.status(400).json({error: 'You are friends already.'});
+    // }
 
-    const [item] = await pool.query('INSERT INTO friendships (user_id, friend_id, status) VALUES (?, ?, ?)', [user_id, request_id, 'pending']);
-    const [friendshipInfo] = await pool.query('SELECT * FROM users WHERE id = ?', [item.insertId])[0];
-    const friendship = { id: friendshipInfo.id };
-    return res.status(200).json({data: {friendship} });
+    // const [item] = await pool.query('INSERT INTO friendships (user_id, friend_id, status) VALUES (?, ?, ?)', [user_id, request_id, 'pending']);
+    // const [friendshipInfo] = await pool.query('SELECT * FROM users WHERE id = ?', [item.insertId])[0];
+    // const friendship = { id: friendshipInfo.id };
+    // return res.status(200).json({data: {friendship} });
 
 });
 
