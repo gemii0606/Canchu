25d5d10aadbd06238b59d48ec7ec8b9ab8a3b6b7
gemii0606 <gemii0606@gmail.com>
2023-07-10 16:30:17 +0800

    Update index.js

--- api/1.0/friends/index.js
+++ api/1.0/friends/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const friendRequestRoute = require('./friends_request');
 // const friendAgreeRoute = require('./friends_agree');
 
-router.use('/:user_id/request', friendRequestRoute);
+router.post('/:user_id/request', friendRequestRoute);
 // router.post('/friends/:friendship_id/agree', friendAgreeRoute);
 
 
