6d77b560f4420cfa0e66118cc937dbcc86b3a2bc
gemii0606 <gemii0606@gmail.com>
2023-07-10 23:52:10 +0800

    Update profile.js

--- api/1.0/users/profile.js
+++ api/1.0/users/profile.js
@@ -12,7 +12,7 @@ router.get('/', checkAuthorization, async (req, res) => {
   // if the authorization passes, user can see others profile
     try {
       // taken id should be int
-      console.log(req.baseUrl.split('/'));
+      console.log(req.baseUrl.split('/').slice(-2));
       const userId = parseInt(req.params.id);
 
       const [userItem] = await pool.query('SELECT * FROM users WHERE id = ?', [userId]);
