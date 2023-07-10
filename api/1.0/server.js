a98d829464c90e24a6f937f7bb7ac3229c8aa4bb
gemii0606 <gemii0606@gmail.com>
2023-07-10 09:48:41 +0800

    Update server.js

--- api/1.0/server.js
+++ api/1.0/server.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(express.json());
+app.use(express.static('public'))
 
 const usersRoute = require('./users');
 app.use('/api/1.0/users', usersRoute);
