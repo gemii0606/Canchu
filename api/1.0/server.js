f2257d0169b20cc9914cfd60b899ebb9db3a19a3
gemii0606 <gemii0606@gmail.com>
2023-07-10 16:16:24 +0800

    Update server.js

--- api/1.0/server.js
+++ api/1.0/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
-
+console.log('server 1');
 const app = express();
+console.log('server 2');
 app.use(express.json());
 app.use(express.static('public'))
 
 const usersRoute = require('./users');
 app.use('/api/1.0/users', usersRoute);
-console.log('server');
+console.log('server 3');
 const friendsRoute = require('./friends');
 app.use('/api/1.0/friends', friendsRoute);
 
