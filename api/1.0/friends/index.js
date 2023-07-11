524315aecee9656440d9720c7b34ead599ab6ddf
gemii0606 <gemii0606@gmail.com>
2023-07-11 17:59:57 +0800

    Update index.js

--- api/1.0/friends/index.js
+++ api/1.0/friends/index.js
@@ -9,6 +9,6 @@ const friendsDeleteRoute = require('./friends_delete');
 router.use('/:user_id/request', friendsRequestRoute);
 router.use('/pending', friendsPendingRoute);
 router.use('/:friendship_id/agree', friendsAgreeRoute);
-router.use('/:friendship_id', friendsAgreeRoute);
+router.use('/:friendship_id', friendsDeleteRoute);
 
 module.exports = router;
