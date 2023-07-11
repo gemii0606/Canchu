c885335dfb49c052ce24bb1e1a83513317a8f19c
gemii0606 <gemii0606@gmail.com>
2023-07-11 17:58:54 +0800

    1

---
+++ api/1.0/friends/friend_delete.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../utils/model/users');
+const Friendship = require('../utils/model/friendships');
+
+// take out the function
+const { checkAuthorization } = require('../utils/function');
+
+
+router.delete('/', checkAuthorization, async (req, res) => {
+    try {
+        const reqFriendshipId = req.baseUrl.split('/').slice(-1);
+        const decodedToken = req.decodedToken;
+        const user_id = decodedToken.id;
+        const delete_id = parseInt(reqFriendshipId); // friendships table's id
+
+        const delete_target = await Friendship.findOne({
+            where: { id: delete_id }
+        });
+
+        if (!delete_target) {
+            return res.status(400).json({ error: 'No relationship is found.' });
+        }
+
+        if (delete_target.to_id === user_id && delete_target.status === 'pending') {
+            return res.status(400).json({ error: 'Your relationship has not connected.' });
+        }
+
+        if (friendship.status === 'friend') {
+
+            const delete_action = await Friendship.destroy({
+                where: { id: delete_id }
+            });
+    
+            return res.status(200).json({ data: {friendship: delete_id}});
+            
+        }
+
+
+    } catch (err) {
+        console.error(`${err.message} `);
+        res.status(500).json({ error: 'Server error' });
+    }
+
+});
+
+module.exports = router;
--- api/1.0/friends/index.js
+++ api/1.0/friends/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const friendsRequestRoute = require('./friends_request');
 const friendsPendingRoute = require('./friends_pending');
 const friendsAgreeRoute = require('./friends_agree');
+const friendsDeleteRoute = require('./friends_delete');
 
 router.use('/:user_id/request', friendsRequestRoute);
 router.use('/pending', friendsPendingRoute);
 router.use('/:friendship_id/agree', friendsAgreeRoute);
+router.use('/:friendship_id', friendsAgreeRoute);
 
 module.exports = router;
