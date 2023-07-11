6fa083076eb7d6c00ee6fd2006b41d6695f21969
gemii0606 <gemii0606@gmail.com>
2023-07-11 12:54:12 +0800

    Update friends_pending.js

--- api/1.0/friends/friends_pending.js
+++ api/1.0/friends/friends_pending.js
@@ -19,6 +19,6 @@ router.post('/', checkAuthorization, async (req, res) => {
 
     console.log(from_pending)
     
-    return res.status(200).json({ data: { friendship: friendshipInfo } });
+    // return res.status(200).json({ data: { friendship: friendshipInfo } });
 
 });
