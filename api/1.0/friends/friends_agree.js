6d9eeeb2ff9bbda67a8fb95415304cb9196478b9
gemii0606 <gemii0606@gmail.com>
2023-07-11 17:05:41 +0800

    Update friends_agree.js

--- api/1.0/friends/friends_agree.js
+++ api/1.0/friends/friends_agree.js
@@ -8,30 +8,34 @@ const { checkAuthorization } = require('../utils/function');
 
 
 router.post('/', checkAuthorization, async (req, res) => {
+    try {
+        const reqFriendshipId = req.baseUrl.split('/').slice(-2,-1)[0];
+        const decodedToken = req.decodedToken;
+        const user_id = decodedToken.id;
+        const friendship_id = parseInt(reqFriendshipId); // friendships table's id
 
-    const reqFriendshipId = req.baseUrl.split('/').slice(-2,-1)[0];
-    const decodedToken = req.decodedToken;
-    const user_id = decodedToken.id;
-    const friendship_id = parseInt(reqFriendshipId); // friendships table's id
+        const friendship = await Friendship.findOne({
+            where: { id: friendship_id }
+        });
 
-    const friendship = await Friendship.findOne({
-        where: { id: friendship_id }
-    });
+        if (!friendship) {
+            return res.status(400).json({ error: 'There is no request.' });
+        }
 
-    if (!friendship) {
-        return res.status(400).json({ error: 'There is no request.' });
+        if (friendship.status === 'requested') {
+            return res.status(400).json({ error: 'You are already friends.' });
+        }
+
+        if (friendship.from_id === user_id) {
+            return res.status(400).json({ error: 'You are not the receiver.' });
+        }
+
+        friendship.status = 'requested';
+        return res.status(200).json({ data: {friendship: friendship_id}});
+    } catch (err) {
+        console.error(`${err.message} `);
+        res.status(500).json({ error: 'Server error' });
     }
-
-    if (friendship.status === 'requested') {
-        return res.status(400).json({ error: 'You are already friends.' });
-    }
-
-    if (friendship.from_id === user_id) {
-        return res.status(400).json({ error: 'You are not the receiver.' });
-    }
-
-    friendship.status = 'requested';
-    return res.status(200).json({ data: {friendship: friendship_id}});
 
 });
 
