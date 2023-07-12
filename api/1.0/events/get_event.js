6e560a388dc54492c8fad52bde65e7b92c7365b0
gemii0606 <gemii0606@gmail.com>
2023-07-12 15:47:41 +0800

    Update get_event.js

--- api/1.0/events/get_event.js
+++ api/1.0/events/get_event.js
@@ -9,37 +9,39 @@ const { checkAuthorization } = require('../utils/function');
 
 
 router.get('/', checkAuthorization, async (req, res) => {
+    try {
+        const decodedToken = req.decodedToken;
+        const user_id = decodedToken.id;
 
-    const decodedToken = req.decodedToken;
-    const user_id = decodedToken.id;
 
-
-    const events_request = await Event.findAll({
-        where: { to_id: user_id },
-        include: [
-            {
-                model: User,
-                attributes: ['picture'],
-                as: 'FromUser'
-            }
-        ]
-    });
-
-    console.log(events_request);
-    console.log(events_request.length);
-    if (events_request.length !== 0) {
-        const events = events_request.map(event =>{
-            const data ={
-                id: event.dataValues.id,
-                type: event.dataValues.type,
-                is_read: event.dataValues.is_read,
-                image: event.dataValues.FromUser.dataValues.picture,
-                created_at: event.dataValues.createdAt,
-                summary: event.dataValues.summary
-                };
-            return data;
+        const events_request = await Event.findAll({
+            where: { to_id: user_id },
+            include: [
+                {
+                    model: User,
+                    attributes: ['picture'],
+                    as: 'FromUser'
+                }
+            ]
         });
-        return res.status(200).json({data: {events}});
+
+        if (events_request.length !== 0) {
+            const events = events_request.map(event =>{
+                const data ={
+                    id: event.dataValues.id,
+                    type: event.dataValues.type,
+                    is_read: event.dataValues.is_read,
+                    image: event.dataValues.FromUser.dataValues.picture,
+                    created_at: event.dataValues.createdAt,
+                    summary: event.dataValues.summary
+                    };
+                return data;
+            });
+            return res.status(200).json({data: {events}});
+    }
+    } catch {
+        console.error(`${err.message} `);
+        res.status(500).json({ error: 'Server error' });
     }
 });
 
