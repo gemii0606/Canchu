30ab03d695718df1b65c157a4e9bcdb771eaba0a
gemii0606 <gemii0606@gmail.com>
2023-07-13 18:28:29 +0800

    Update friends_get.js

--- api/1.0/friends/friends_get.js
+++ api/1.0/friends/friends_get.js
@@ -12,15 +12,27 @@ router.get('/', checkAuthorization, async (req, res) => {
     const decodedToken = req.decodedToken;
     const user_id = decodedToken.id;  // see if you are receiver
 
-    const users = await Friendship.findAll({
-        where: {
-          [Op.and]: [
-            { [Op.or]: [{ from_id: user_id }, { to_id: user_id }] },
-            { status: 'friend' }
-          ]
-        }
+    // const users = await Friendship.findAll({
+    //     where: {
+    //       [Op.and]: [
+    //         { [Op.or]: [{ from_id: user_id }, { to_id: user_id }] },
+    //         { status: 'friend' }
+    //       ]
+    //     }
+    //   });
+    const users = await User.findAll({
+        attributes: ['id', 'Username', 'Picture'],
+        include: [
+          {
+            model: Friendship,
+            attributes: ['id', 'status'],
+            where: {
+              status: 'friend'
+            }
+          }
+        ]
       });
-    
+      console.log(users);
     
 });
 
