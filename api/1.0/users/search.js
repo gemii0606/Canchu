bb1f4f033ce5964647c70f128d68eb66be1569f3
gemii0606 <gemii0606@gmail.com>
2023-07-13 11:04:42 +0800

    1

---
+++ api/1.0/users/search.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../utils/model/users');
+const Friendship = require('../utils/model/friendships');
+
+// take out the function
+const { checkAuthorization } = require('../utils/function');
+
+router.get('/', checkAuthorization, async (req, res) => {
+
+});
+
+module.exports = router;
