d513d4be99476c3a6374052cdb0c9dfb2512d03d
gemii0606 <gemii0606@gmail.com>
2023-07-13 16:30:26 +0800

    Update search.js

--- api/1.0/users/search.js
+++ api/1.0/users/search.js
@@ -13,76 +13,98 @@ router.get('/', checkAuthorization, async (req, res) => {
     const { keyword } = req.query;
 
 
-    const query_users = await User.findAll({
+    // const query_users = await User.findAll({
+    //   where: {
+    //     id: {
+    //       [Op.not]: user_id
+    //     },
+    //     name: {
+    //       [Op.like]: `%${keyword}%`
+    //     }
+    //   },
+    //   attributes: ['id', 'name', 'picture']
+    // });
+
+    // let users = [];
+
+    // for (const other of query_users) {
+    //   const [friendship] = await Friendship.findAll({
+    //     where: {
+    //       [Op.or]: [
+    //         { [Op.and]: [{ from_id: user_id }, { to_id: other.id }] },
+    //         { [Op.and]: [{ from_id: other.id }, { to_id: user_id }] }
+    //       ]
+    //     }
+    //   });    
+
+    //   if (!friendship) {
+    //     const data = {
+    //       id: other.dataValues.id,
+    //       name: other.dataValues.name,
+    //       picture: other.dataValues.picture,
+    //       friendship: null
+    //     };
+    //     users.push(data);
+    //     continue;
+    //   } else if ( friendship.dataValues.from_id === user_id && friendship.dataValues.status === 'pending') {
+    //     const data = {
+    //       id: other.dataValues.id,
+    //       name: other.dataValues.name,
+    //       picture: other.dataValues.picture,
+    //       friendship: {
+    //         id: friendship.dataValues.id,
+    //         status: 'requested'
+    //       }
+    //     };
+    //     users.push(data);
+    //   } else if ( friendship.dataValues.to_id === user_id && friendship.dataValues.status === 'pending') {
+    //     const data = {
+    //       id: other.dataValues.id,
+    //       name: other.dataValues.name,
+    //       picture: other.dataValues.picture,
+    //       friendship: {
+    //         id: friendship.dataValues.id,
+    //         status: 'pending'
+    //       }
+    //     };
+    //     users.push(data);
+    //   } else if ( friendship.dataValues.status === 'friend') {
+    //     const data = {
+    //       id: other.dataValues.id,
+    //       name: other.dataValues.name,
+    //       picture: other.dataValues.picture,
+    //       friendship: {
+    //         id: friendship.dataValues.id,
+    //         status: 'friend'
+    //       }
+    //     };
+    //     users.push(data);
+    //   }
+    // }
+
+    const users = await User.findAll({
+      attributes: [
+        ['id', 'id'],
+        ['Username', 'name'],
+        ['Picture', 'picture'],
+        [sequelize.literal(`CASE
+            WHEN Friendship.user_id = ${user_id} AND Friendship.friend_id = Users.id THEN 'requested'
+            WHEN Friendship.user_id = Users.id AND Friendship.friend_id = ${user_id} THEN 'pending'
+          END`), 'friendship_status']
+      ],
+      include: {
+        model: Friendship,
+        required: false,
+        attributes: []
+      },
       where: {
-        id: {
-          [Op.not]: user_id
-        },
-        name: {
-          [Op.like]: `%${keyword}%`
+        Username: {
+          [Sequelize.Op.like]: `%${keyword}%`
         }
       },
-      attributes: ['id', 'name', 'picture']
+      raw: true
     });
 
-    let users = [];
-
-    for (const other of query_users) {
-      console.log(other.id)
-
-      const [friendship] = await Friendship.findAll({
-        where: {
-          [Op.or]: [
-            { [Op.and]: [{ from_id: user_id }, { to_id: other.id }] },
-            { [Op.and]: [{ from_id: other.id }, { to_id: user_id }] }
-          ]
-        }
-      });    
-      console.log(friendship);
-      if (!friendship) {
-        const data = {
-          id: other.dataValues.id,
-          name: other.dataValues.name,
-          picture: other.dataValues.picture,
-          friendship: null
-        };
-        users.push(data);
-        continue;
-      } else if ( friendship.dataValues.from_id === user_id && friendship.dataValues.status === 'pending') {
-        const data = {
-          id: other.dataValues.id,
-          name: other.dataValues.name,
-          picture: other.dataValues.picture,
-          friendship: {
-            id: friendship.dataValues.id,
-            status: 'requested'
-          }
-        };
-        users.push(data);
-      } else if ( friendship.dataValues.to_id === user_id && friendship.dataValues.status === 'pending') {
-        const data = {
-          id: other.dataValues.id,
-          name: other.dataValues.name,
-          picture: other.dataValues.picture,
-          friendship: {
-            id: friendship.dataValues.id,
-            status: 'pending'
-          }
-        };
-        users.push(data);
-      } else if ( friendship.dataValues.status === 'friend') {
-        const data = {
-          id: other.dataValues.id,
-          name: other.dataValues.name,
-          picture: other.dataValues.picture,
-          friendship: {
-            id: friendship.dataValues.id,
-            status: 'friend'
-          }
-        };
-        users.push(data);
-      }
-    }
     return res.status(200).json({data: {users: users}});
 });
 
