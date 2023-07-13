d82ddf8e63f2ff8b24df421973612c66920520f0
gemii0606 <gemii0606@gmail.com>
2023-07-13 16:36:08 +0800

    Update search.js

--- api/1.0/users/search.js
+++ api/1.0/users/search.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../utils/model/users');
 const Friendship = require('../utils/model/friendships');
-const { Op } = require('sequelize');
+const { Sequelize, Op } = require('sequelize');
 
 // take out the function
 const { checkAuthorization } = require('../utils/function');
@@ -87,7 +87,7 @@ router.get('/', checkAuthorization, async (req, res) => {
         ['id', 'id'],
         ['Username', 'name'],
         ['Picture', 'picture'],
-        [sequelize.literal(`CASE
+        [Sequelize.literal(`CASE
             WHEN Friendship.user_id = ${user_id} AND Friendship.friend_id = Users.id THEN 'requested'
             WHEN Friendship.user_id = Users.id AND Friendship.friend_id = ${user_id} THEN 'pending'
           END`), 'friendship_status']
