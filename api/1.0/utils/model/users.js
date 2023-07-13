ec83603bfbc139ba161fdfcfe095c343a88974e8
gemii0606 <gemii0606@gmail.com>
2023-07-13 10:34:34 +0800

    Update users.js

--- api/1.0/utils/model/users.js
+++ api/1.0/utils/model/users.js
@@ -11,10 +11,10 @@ const sequelize = new Sequelize('canchu', 'gemii0606', process.env.SQL_NODEJS_PW
 
 const User = sequelize.define('User', {}, { tableName: 'users', timestamps: false });
 
-User.hasMany(Friendship, { foreignKey: 'from_id', sourceKey: 'id' })
-User.hasMany(Friendship, { foreignKey: 'to_id', sourceKey: 'id' })
-User.hasMany(Event, { foreignKey: 'from_id', sourceKey: 'id' })
-User.hasMany(Event, { foreignKey: 'to_id', sourceKey: 'id' })
+// User.hasMany(Friendship, { foreignKey: 'from_id', sourceKey: 'id' })
+// User.hasMany(Friendship, { foreignKey: 'to_id', sourceKey: 'id' })
+// User.hasMany(Event, { foreignKey: 'from_id', sourceKey: 'id' })
+// User.hasMany(Event, { foreignKey: 'to_id', sourceKey: 'id' })
 
 User.sync()
 
