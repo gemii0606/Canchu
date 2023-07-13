52b5904ad4c58f466f24c7e2449b17f5da15d882
gemii0606 <gemii0606@gmail.com>
2023-07-13 23:15:08 +0800

    Update model.js

--- api/1.0/utils/models/model.js
+++ api/1.0/utils/models/model.js
@@ -3,34 +3,55 @@ const Friendship = require('./friendships');
 const Event = require('./events');
 
 
-Friendship.associate = function(models) {
-    Friendship.belongsTo(User, { foreignKey: 'from_id', targetKey: 'id', as: 'FromUser' });
-    Friendship.belongsTo(User, { foreignKey: 'to_id', targetKey: 'id', as: 'ToUser' });
-}
+// Friendship.associate = function(models) {
+//     Friendship.belongsTo(User, { foreignKey: 'from_id', targetKey: 'id', as: 'FromUser' });
+//     Friendship.belongsTo(User, { foreignKey: 'to_id', targetKey: 'id', as: 'ToUser' });
+// }
   
-Event.belongsTo(User, { foreignKey: 'from_id', targetKey: 'id', as: 'EventFromUser' })
-Event.belongsTo(User, { foreignKey: 'to_id', targetKey: 'id', as: 'EventToUser' })
+// Event.belongsTo(User, { foreignKey: 'from_id', targetKey: 'id', as: 'EventFromUser' })
+// Event.belongsTo(User, { foreignKey: 'to_id', targetKey: 'id', as: 'EventToUser' })
 
 
-User.associate = function(models) {
-    // User.hasMany(Friendship, { foreignKey: 'from_id', sourceKey: 'id' });
-    // User.hasMany(Friendship, { foreignKey: 'to_id', sourceKey: 'id' });
-    // User.hasMany(Event, { foreignKey: 'from_id', sourceKey: 'id' });
-    // User.hasMany(Event, { foreignKey: 'to_id', sourceKey: 'id' });
-    User.hasMany(Friendship);
-    // User.belongsTo(Friendship, { foreignKey: 'to_id', sourceKey: 'id' });
-    // User.belongsTo(Event, { foreignKey: 'from_id', sourceKey: 'id' });
-    // User.belongsTo(Event, { foreignKey: 'to_id', sourceKey: 'id' });
-  }
+// User.associate = function(models) {
+//     User.hasMany(Friendship, { foreignKey: 'from_id', sourceKey: 'id' });
+//     User.hasMany(Friendship, { foreignKey: 'to_id', sourceKey: 'id' });
+//     User.hasMany(Event, { foreignKey: 'from_id', sourceKey: 'id' });
+//     User.hasMany(Event, { foreignKey: 'to_id', sourceKey: 'id' });
+//   }
 
 
 User.sync().then(() => {
-    User.hasMany(Friendship, { foreignKey: 'id' });
+    User.associate = function(models) {
+        User.hasMany(Friendship, { foreignKey: 'from_id', sourceKey: 'id' });
+        User.hasMany(Friendship, { foreignKey: 'to_id', sourceKey: 'id' });
+        User.hasMany(Event, { foreignKey: 'from_id', sourceKey: 'id' });
+        User.hasMany(Event, { foreignKey: 'to_id', sourceKey: 'id' });
+      }
 }).catch(error => {
     console.error('Error syncing :', error);
 });
 
+Friendship.sync().then(() => {
+    Friendship.associate = function(models) {
+        Friendship.associate = function(models) {
+            Friendship.belongsTo(User, { foreignKey: 'from_id', targetKey: 'id', as: 'FromUser' });
+            Friendship.belongsTo(User, { foreignKey: 'to_id', targetKey: 'id', as: 'ToUser' });
+        }
+      }
+}).catch(error => {
+    console.error('Error syncing :', error);
+});
 
+Event.sync().then(() => {
+    Event.associate = function(models) {
+        Event.associate = function(models) {
+            Event.belongsTo(User, { foreignKey: 'from_id', targetKey: 'id', as: 'EventFromUser' })
+            Event.belongsTo(User, { foreignKey: 'to_id', targetKey: 'id', as: 'EventToUser' })
+        }
+      }
+}).catch(error => {
+    console.error('Error syncing :', error);
+});
 
 
 module.exports = {
