7a48e1e5f6c25c6f8190752e5564998aedc093c9
gemii0606 <gemii0606@gmail.com>
2023-07-14 00:59:12 +0800

    Update friends_get.js

--- api/1.0/friends/friends_get.js
+++ api/1.0/friends/friends_get.js
@@ -21,13 +21,12 @@ router.get('/', checkAuthorization, async (req, res) => {
     //     }
     //   });
     const users = await User.findAll({
-        where: { id: 5},
+        where: { id: 10},
         attributes: ['id', 'name', 'picture'],
         include: [
           {
             model: Friendship,
-            // attributes: ['id', 'from_id', 'status']
-            as: 'fr'
+            attributes: ['id', 'from_id', 'status']
           }
         ]
       });
