086a1a602770e90c65ad089df75d62af751806da
gemii0606 <gemii0606@gmail.com>
2023-07-14 17:33:30 +0800

    Update update_detail_post.js

--- api/1.0/posts/update_detail_post.js
+++ api/1.0/posts/update_detail_post.js
@@ -38,12 +38,12 @@ router.put('/', checkAuthorization, async (req, res) => {
 });
 
 router.get('/', checkAuthorization, async (req, res) => {
-    const post_id = req.baseUrl.split('/').slice(-1);
+    const search_post_id = req.baseUrl.split('/').slice(-1);
     const decodedToken = req.decodedToken;
     // const user_id = decodedToken.id;  // see if you are receiver
  
-    const post = await Post.findOne({
-        where: { id: post_id },
+    const result_1 = await Post.findOne({
+        where: { id: search_post_id },
         attributes: ['id', 'user_id', 'context', 'createdAt'],
         include: [
             {
@@ -58,30 +58,58 @@ router.get('/', checkAuthorization, async (req, res) => {
             }
         ]
     });
-      
-    //   // 格式化回應資料
-    //   const formattedPost = {
-    //     id: post.id,
-    //     created_at: post.created_at,
-    //     context: post.context,
-    //     is_liked: true, // TODO: 根據實際邏輯填入
-    //     like_count: post.postLike.length, // 假設已建立 Like 的關聯
-    //     comment_count: post.postComment.length, // 假設已建立 Comment 的關聯
-    //     picture: post.picture,
-    //     name: post.postUser.name,
-    //     comments: post.postComment.map(comment => ({
-    //       id: comment.id,
-    //       created_at: comment.created_at,
-    //       content: comment.content,
-    //       user: {
-    //         id: comment.commentUser.id,
-    //         name: comment.commentUser.name,
-    //         picture: comment.commentUser.picture
-    //       }
-    //     }))
-    //   };
-      
-      return res.status(200).json(post);
+
+    const result_2 = await User.findOne({
+        where: { id: result_1.user_id },
+        attributes: ['name', 'picture'],
+    });
+
+    const result_3 = await Like.findOne({
+        where: { id: result_1.user_id, post_id: search_post_id}
+    });
+
+    const post_id = result_1.id;
+    const post_created_at = result_1.createdAt;
+    const post_context = result_1.context;
+    const post_is_like = (!!result_3);
+    const post_like_count = result_1.postLike.length;
+    const post_comment_count = result_1.postComment.length;
+    const post_picture = result_2.picture;
+    const post_name = result_2.name;
+    
+    const result_4 = result_1.postComment.map(async (element) => {
+        const user_info = await User.findOne({
+            where: {id: element.commenter_id},
+            attributes: ['id', 'name', 'picture']
+        });
+        const outcome = {
+            id: element.id,
+            created_at: element.createdAt,
+            content: element.content,
+            user: {
+                id: user_info.id,
+                name: user_info.name,
+                picture: user_info.picture
+            }
+        };
+        return outcome;
+    });
+
+    const data = {
+        post:{
+            id: post_id,
+            created_at: post_created_at,
+            context: post_context,
+            is_liked: post_is_like,
+            like_count: post_like_count,
+            comment_count: post_comment_count,
+            picture: post_picture,
+            name: post_name,
+            comments: result_4
+        }
+    };  
+    
+    return res.status(200).json({data});
 });
 
 
