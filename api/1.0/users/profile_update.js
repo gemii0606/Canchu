c049cd37b173e8cf1c8bdd4eb37413052e821a32
gemii0606 <gemii0606@gmail.com>
2023-07-17 10:53:38 +0800

    Update profile_update.js

--- api/1.0/users/profile_update.js
+++ api/1.0/users/profile_update.js
@@ -19,7 +19,6 @@ router.put('/', checkAuthorization, async (req, res) => {
       if (!(name || introduction || tags)) {
         return res.status(400).json({ error: 'You should make one change at least.' });
       }
-      // const [result] = await pool.query('UPDATE users SET name = ?, introduction = ?, tags = ? WHERE id = ?', [name, introduction, tags, id]);
       
       const userInfo = await User.findOne({
         where: {
