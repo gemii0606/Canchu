58741519d8f2910e82cb76b582d5aab7ce231a94
gemii0606 <gemii0606@gmail.com>
2023-07-17 14:33:32 +0800

    1

--- api/1.0/posts/index.js
+++ api/1.0/posts/index.js
@@ -5,7 +5,9 @@ const createPostRoute = require('./create_post');
 const updateDetailPostRoute = require('./update_detail_post');
 const likePostRoute = require('./like_post');
 const commentPostRoute = require('./comment_post');
+const searchPostRoute = require('./search_post');
 
+router.use('/search', searchPostRoute);
 router.use('/:id/comment', commentPostRoute);
 router.use('/:id/like', likePostRoute);
 router.use('/:id', updateDetailPostRoute);
---
+++ api/1.0/posts/search_post.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = express.Router();
+const {User, Post, Like, Comment} = require('../utils/models/model');
+const { Op } = require('sequelize');
+const moment = require('moment');
+
+// take out the function
+const { checkAuthorization } = require('../utils/function');
+
+
+router.get('/', checkAuthorization, async (req, res) => {
+    const decodedToken = req.decodedToken;
+    const id = decodedToken.id;  // see if you are receiver
+
+    const { user_id, cursor } = req.query;
+
+    let currentPage;
+    if (cursor) {
+        currentPage = parseInt(atob(cursor));
+    } else {
+        currentPage = 1;
+    }
+    
+    const pageSize = 10;
+
+    const whereClause = {};
+
+    if (user_id) {
+      whereClause.user_id = user_id;
+    } else {
+      whereClause.user_id = id;
+    }
+
+    // 設置分頁的選項
+    const options = {
+      where: whereClause,
+      limit: pageSize + 1, // 加1是為了檢查是否有下一頁
+    };
+
+    if (cursor) {
+        options.where.cursor = { [Op.gt]: currentPage };
+    
+    }
+
+    // 查詢數據庫
+    const { count, rows } = await Post.findAndCountAll({
+        where: {
+          options
+        },
+        offset: (currentPage - 1) * pageSize,
+        limit: pageSize,
+      });
+
+    const totalPages = Math.ceil(count / pageSize);
+    const hasNextPage = currentPage < totalPages;
+
+    const result = {
+        posts: rows,
+        totalPosts: count,
+        currentPage,
+        totalPages,
+        hasNextPage,
+      };
+    
+    const data ={
+        posts: result.posts,
+        next_cursor: btoa((result.currentPage + 1).toString())
+    };
+    // 返回結果
+    return res.status(200).json({ data });
+
+});
+
+
+module.exports = router;
