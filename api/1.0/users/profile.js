b7d8fa105076e6bd7c10259e09b4d34987a843c1
gemii0606 <gemii0606@gmail.com>
2023-07-18 10:58:32 +0800

    Update profile.js

--- api/1.0/users/profile.js
+++ api/1.0/users/profile.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const {User} = require('../utils/models/model');
+const {User, Friendship} = require('../utils/models/model');
+const { Op } = require('sequelize');
 const router = express.Router();
 
 // take out the function
@@ -11,13 +12,58 @@ router.get('/', checkAuthorization, async (req, res) => {
       // taken id from its baseUrl property, if /:id/profile, then :id should be in the -2 position
       const reqId = req.baseUrl.split('/').slice(-2,-1)[0];
       const userId = parseInt(reqId);
+      const decodedToken = req.decodedToken;
+      const id = decodedToken.id;  // see if you are receiver
 
       const userInfo = await User.findOne({
         where: {
             id: userId
         },
-        attributes: ['id', 'name', 'picture', 'introduction', 'tags']
+        attributes: ['id', 'name', 'picture', 'introduction', 'tags'],
+        include: [
+          {
+            model: Friendship,
+            as: 'fromFriendship',
+            where:{to_id: id},
+            attributes: ['id', 'status']
+          },
+          {
+            model: Friendship,
+            as: 'toFriendship',
+            where:{from_id: id},
+            attributes: ['id', 'status']
+          }
+        ],
+        required: false
       });
+      console.log(userInfo)
+      // if (userInfo.fromFriendship)
+
+      // const result = [];
+      
+      // if (userInfo.fromFriendship.length > 0) {
+      //   for (const friend of userInfo.fromFriendship) {
+      //     let userObj = {};
+      //     userObj.id = friend.toUser.id;
+      //     userObj.name = friend.toUser.name;
+      //     userObj.picture = friend.toUser.picture;
+      //     userObj.friendship = { 
+      //       id: friend.id,
+      //       status: friend.status
+      //     };
+      //     result.push(userObj);
+      //     }
+      // } 
+      
+      // if (userInfo.toFriendship.length > 0) {
+      //   for (const friend of userInfo.toFriendship) {
+      //     let userObj = {
+      //       id: friend.id,
+      //       status: friend.status
+      //     };
+      //     result.push(userObj);
+      //   }
+      // }
 
       const user = {
           id: userInfo.id,
