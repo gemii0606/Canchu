16571cfac202d80eba6c8c4338a548d616949cca
gemii0606 <gemii0606@gmail.com>
2023-07-19 10:02:06 +0800

    Update friends_delete.js

--- api/1.0/friends/friends_delete.js
+++ api/1.0/friends/friends_delete.js
@@ -21,19 +21,19 @@ router.delete('/', checkAuthorization, async (req, res) => {
             return res.status(400).json({ error: 'No relationship is found.' });
         }
 
-        if (delete_target.to_id === user_id && delete_target.status === 'pending') {
-            return res.status(400).json({ error: 'Your relationship has not connected.' });
-        }
+        // if (delete_target.to_id === user_id && delete_target.status === 'pending') {
+        //     return res.status(400).json({ error: 'Your relationship has not connected.' });
+        // }
 
-        if (delete_target.status === 'friend') {
+        // if (delete_target.status === 'friend') {
 
-            const delete_action = await Friendship.destroy({
-                where: { id: delete_id }
-            });
+        //     const delete_action = await Friendship.destroy({
+        //         where: { id: delete_id }
+        //     });
     
-            return res.status(200).json({ data: {friendship: delete_id}});
+        //     return res.status(200).json({ data: {friendship: delete_id}});
             
-        }
+        // }
         const delete_action = await Friendship.destroy({
             where: { id: delete_id }
         });
