0772af13ec2c9148337937da281fc70fa1870697
gemii0606 <gemii0606@gmail.com>
2023-07-19 14:57:20 +0800

    1

--- api/1.0/users/profile.js
+++ api/1.0/users/profile.js
@@ -8,7 +8,7 @@ const { checkAuthorization } = require('../utils/function');
   
 router.get('/', checkAuthorization, async (req, res) => {
   // if the authorization passes, user can see others profile
-    try {
+    // try {
       // taken id from its baseUrl property, if /:id/profile, then :id should be in the -2 position
       const reqId = req.baseUrl.split('/').slice(-2,-1)[0];
       const userId = parseInt(reqId);
@@ -71,10 +71,10 @@ router.get('/', checkAuthorization, async (req, res) => {
       
       return res.status(200).json({ data: {user} });
 
-    } catch (err) {
-        console.error(`${err.message} `);
-        return res.status(500).json({ error: 'Server error' });
-    }
+    // } catch (err) {
+    //     console.error(`${err.message} `);
+    //     return res.status(500).json({ error: 'Server error' });
+    // }
  });
 
 module.exports = router;
