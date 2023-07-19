320f651862c5e9adde70c0f8a5a1731147df3ee5
gemii0606 <gemii0606@gmail.com>
2023-07-19 10:22:27 +0800

    Update index.js

--- api/1.0/events/index.js
+++ api/1.0/events/index.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const eventGetRoute = require('./get_event');
 const eventReadRoute = require('./read_event');
 
-router.use('/:event_id/read', eventReadRoute);
 router.use('/', eventGetRoute);
+
+router.use('/:event_id/read', eventReadRoute);
 
 
 module.exports = router;
