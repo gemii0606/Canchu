cc8afc54965b2864f89768f2db553169157ed7dc
gemii0606 <gemii0606@gmail.com>
2023-07-24 14:46:29 +0800

    Update server.js

--- api/1.0/server.js
+++ api/1.0/server.js
@@ -5,16 +5,16 @@ const usersRoute = require('./users');
 const friendsRoute = require('./friends');
 const eventsRoute = require('./events');
 const postsRoute = require('./posts');
-const rateLimit = require('express-rate-limit');
+// const rateLimit = require('express-rate-limit');
 
 
-const limiter = rateLimit({
-  windowMs: 10 * 1000, 
-  max: 100
-});
+// const limiter = rateLimit({
+//   windowMs: 10 * 1000, 
+//   max: 100
+// });
 
 
-app.use(limiter);
+// app.use(limiter);
 
 app.use(cors());
 app.use(express.json());
