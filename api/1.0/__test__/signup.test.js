e70b89126966915255105a097656604fa3d5722d
gemii0606 <gemii0606@gmail.com>
2023-07-24 17:55:31 +0800

    Update signup.test.js

--- api/1.0/__test__/signup.test.js
+++ api/1.0/__test__/signup.test.js
@@ -1,6 +1,6 @@
 // __tests__/signup.test.js
 const request = require('supertest');
-const app = require('../app'); // 假設你的Express應用程序主文件名為app.js
+const app = require('../server.js'); // 假設你的Express應用程序主文件名為app.js
 const { User } = require('../utils/models/model');
 
 describe('User API', () => {
