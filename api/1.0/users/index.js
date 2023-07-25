5325a01424b8051b7c6065a2272b63c369a25c57
gemii0606 <gemii0606@gmail.com>
2023-07-25 15:05:21 +0800

    1

--- api/1.0/users/index.js
+++ api/1.0/users/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // const signinRoute = require('./signin');
 const profileRoute = require('./profile');
-const profileUpdateRoute = require('./profile_update');
+// const profileUpdateRoute = require('./profile_update');
 const picetureUpdateRoute = require('./picture_update');
 const searchRoute = require('./search');
 
@@ -11,14 +11,16 @@ const searchRoute = require('./search');
 
 // router.use('/signin', signinRoute);
 router.use('/:id/profile', profileRoute);
-router.use('/profile', profileUpdateRoute);
+// router.use('/profile', profileUpdateRoute);
 router.use('/picture', picetureUpdateRoute);
 router.use('/search', searchRoute);
 
 const { checkAuthorization } = require('../utils/function');
-const {ErrorHandling, signUpUser, signInUser} = require('../controller/users_controller')
+const {ErrorHandling, signUpUser, signInUser, getUserProfile} = require('../controller/users_controller')
 
 router.post('/signup', (req, res) => {ErrorHandling(signUpUser(req, res), res)});
 router.post('/signin', (req, res) => {ErrorHandling(signInUser(req, res), res)});
+router.get('/', checkAuthorization, (req, res) => {ErrorHandling(getUserProfile(req, res), res)});
+
 
 module.exports = router;
