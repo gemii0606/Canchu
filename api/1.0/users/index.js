feb94540d24254e6f079b499fa864d6ffa41b6f3
gemii0606 <gemii0606@gmail.com>
2023-07-25 14:42:36 +0800

    1

--- api/1.0/users/index.js
+++ api/1.0/users/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const signinRoute = require('./signin');
+// const signinRoute = require('./signin');
 const profileRoute = require('./profile');
 const profileUpdateRoute = require('./profile_update');
 const picetureUpdateRoute = require('./picture_update');
@@ -9,14 +9,15 @@ const searchRoute = require('./search');
 
 
 
-router.use('/signin', signinRoute);
+// router.use('/signin', signinRoute);
 router.use('/:id/profile', profileRoute);
 router.use('/profile', profileUpdateRoute);
 router.use('/picture', picetureUpdateRoute);
 router.use('/search', searchRoute);
 
-const {ErrorHandling, signUpUser} = require('../controller/users_controller')
+const {ErrorHandling, signUpUser, signInUser} = require('../controller/users_controller')
 
 router.post('/signup', (req, res) => {ErrorHandling(signUpUser(req, res), res)});
+router.post('/signin', (req, res) => {ErrorHandling(signInUser(req, res), res)});
 
 module.exports = router;
