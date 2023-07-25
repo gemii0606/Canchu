2676e584c6d0a3ad39a5f3b5a368c8012485c9c8
gemii0606 <gemii0606@gmail.com>
2023-07-25 18:09:33 +0800

    Update index.js

--- api/1.0/posts/index.js
+++ api/1.0/posts/index.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// const createPostRoute = require('./create_post');
-// const updateDetailPostRoute = require('./update_detail_post');
-// const likePostRoute = require('./like_post');
-// const commentPostRoute = require('./comment_post');
-// const searchPostRoute = require('./search_post');
-
 const { ErrorHandling, checkAuthorization } = require('../utils/function');
 const { postCreate, postComment, postLike, postLikeDelete, postSearch, postUpdate, postDetail } = require('../controller/posts_controller')
 
@@ -17,12 +11,6 @@ router.delete('/:id/like', checkAuthorization, async (req, res) => {ErrorHandlin
 router.get('/:id', checkAuthorization, async (req, res) => {ErrorHandling(postDetail(req, res), res)});
 router.put('/:id', checkAuthorization, async (req, res) => {ErrorHandling(postUpdate(req, res), res)});
 router.post('/', checkAuthorization, async (req, res) => {ErrorHandling(postCreate(req, res), res)});
-
-// router.use('/search', searchPostRoute);
-// router.use('/:id/comment', commentPostRoute);
-// router.use('/:id/like', likePostRoute);
-// router.use('/:id', updateDetailPostRoute);
-// router.use('/', createPostRoute);
 
 
 module.exports = router;
