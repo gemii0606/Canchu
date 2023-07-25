3250e8bb89a411f42bf3a80ed63a808d87a2647c
gemii0606 <gemii0606@gmail.com>
2023-07-25 13:42:14 +0800

    Update signup.js

--- api/1.0/users/signup.js
+++ api/1.0/users/signup.js
@@ -9,66 +9,69 @@ function isValidEmail(email) {
     return re.test(email);
 }
 
-async function signUpUser(res, name, email, password) {
-  if (!(name && email && password)) {
-    return res.status(400).json({error: 'You should not leave empty!'});
-  }
-
-  if (!isValidEmail(email)) {
-    return res.status(400).json({error: 'Please fill the correct email adress!'});
-  }
-
-  const select = await User.findOne({
-    where: {
-      email
-    }
-  });
-
-  if (!!select) {
-    return res.status(403).json({error: 'email adress has already exist!'});
-  }
-
-  const securePassword = crypto
-    .createHash('sha256')
-    .update(password + email)
-    .digest('base64');
-
-  const user = await User.create({
-    name,
-    email,
-    password: securePassword,
-    provider: "native",
-    picture: null
-  });
-
-  const payload = {
-    id: user.id,
-    provider: user.provider,
-    name: user.name,
-    email: user.email,
-    picture: user.picture
-  };
-
-  const jwtSecret = 'Secret';
-  const accessToken = jwt.sign(payload, jwtSecret);
-
-  return {
-    access_token: accessToken,
-    user: payload
-  };
-}
-
 router.post('/', async (req, res) => {
     // for Sign-up finction
     try {
-      const { name, email, password } = req.body;
-      const result = await signUpUser(name, email, password);
-      res.status(200).json({ data: result });
-    } catch (err) {
-      // Error handling, return server error response
-      console.error(`${err.message} `);
-      res.status(500).json({ error: 'Server error' });
-    }
+      const {name, email, password} = req.body;    
+        if (!(name && email && password)) { 
+          return res.status(400).json({error: 'You should not leave empty!'});
+        }
+    
+        if (!isValidEmail(email)) {
+          return res.status(400).json({error: 'Please fill the correct email adress!'});
+        }
+        
+        // check if the user had already had an account
+        const select = await User.findOne({
+          where: {
+              email
+          }
+        });
+
+        if (!!select) {
+          return res.status(403).json({error: 'email adress has already exist!'});
+        }
+
+        // encrypt the user password
+        const securePassword = crypto
+            .createHash('sha256')
+            .update(password + email)
+            .digest('base64');
+ 
+        // Create a new user
+        const user = await User.create({
+          name,
+          email,
+          password: securePassword,
+          provider: "native",
+          picture: null
+        });
+
+        // Create the JWT Token Payload
+        let payload = {
+          id: user.id,
+          provider: user.provider,
+          name: user.name,
+          email: user.email,
+          picture: user.picture
+        };
+    
+        // Sign the Access Token using JWT
+        const jwtSecret = 'Secret';
+        let accessToken = jwt.sign(payload, jwtSecret);
+    
+        // Return the successful signup response
+        res.status(200).json({
+          data: {
+            access_token: accessToken,
+            user: payload,
+          },
+        });
+      } catch (err) {
+        // Error handling, return server error response
+        console.error(`${err.message} `);
+        res.status(500).json({ error: 'Server error' });
+      }
     });
 
 module.exports = router;
