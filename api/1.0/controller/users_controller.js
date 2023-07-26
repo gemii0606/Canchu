a06e3fe14ae6389d065a8c9438d8f4bac9fe21ec
gemii0606 <gemii0606@gmail.com>
2023-07-26 15:33:02 +0800

    Update users_controller.js

--- api/1.0/controller/users_controller.js
+++ api/1.0/controller/users_controller.js
@@ -12,65 +12,62 @@ function isValidEmail(email) {
 }
 
 const signUpUser = async (req, res) => {
-  const {name, email, password} = req.body; 
-  console.log(name)
-  console.log(email)
-  console.log(password)
-        if (!(name && email && password)) { 
-          return res.status(400).json({error: 'You should not leave empty!'});
-        }
-    
-        if (!isValidEmail(email)) {
-          return res.status(400).json({error: 'Please fill the correct email adress!'});
-        }
-        
-        // check if the user had already had an account
-        const select = await User.findOne({
-          where: {
-              email
-          }
-        });
+  const {name, email, password} = req.body;
+  if (!(name && email && password)) { 
+    return res.status(400).json({error: 'You should not leave empty!'});
+  }
 
-        if (!!select) {
-          return res.status(403).json({error: 'email adress has already exist!'});
-        }
+  if (!isValidEmail(email)) {
+    return res.status(400).json({error: 'Please fill the correct email adress!'});
+  }
+  
+  // check if the user had already had an account
+  const select = await User.findOne({
+    where: {
+        email
+    }
+  });
 
-        // encrypt the user password
-        const securePassword = crypto
-            .createHash('sha256')
-            .update(password + email)
-            .digest('base64');
- 
-        // Create a new user
-        const user = await User.create({
-          name,
-          email,
-          password: securePassword,
-          provider: "native",
-          picture: null
-        });
+  if (!!select) {
+    return res.status(403).json({error: 'email adress has already exist!'});
+  }
 
-        // Create the JWT Token Payload
-        let payload = {
-          id: user.id,
-          provider: user.provider,
-          name: user.name,
-          email: user.email,
-          picture: user.picture
-        };
-    
-        // Sign the Access Token using JWT
-        const jwtSecret = 'Secret';
-        let accessToken = jwt.sign(payload, jwtSecret);
-    
-        // Return the successful signup response
-        res.status(200).json({
-          data: {
-            access_token: accessToken,
-            user: payload,
-          },
-        });
-      }
+  // encrypt the user password
+  const securePassword = crypto
+      .createHash('sha256')
+      .update(password + email)
+      .digest('base64');
+
+  // Create a new user
+  const user = await User.create({
+    name,
+    email,
+    password: securePassword,
+    provider: "native",
+    picture: null
+  });
+
+  // Create the JWT Token Payload
+  let payload = {
+    id: user.id,
+    provider: user.provider,
+    name: user.name,
+    email: user.email,
+    picture: user.picture
+  };
+
+  // Sign the Access Token using JWT
+  const jwtSecret = 'Secret';
+  let accessToken = jwt.sign(payload, jwtSecret);
+
+  // Return the successful signup response
+  res.status(200).json({
+    data: {
+      access_token: accessToken,
+      user: payload,
+    },
+  });
+}
 
 // Function for handling user sign-in with native method
 async function signInNative(object, res) {
