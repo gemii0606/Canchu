55f72257a0901f3a60310282ad52473254101cb3
gemii0606 <gemii0606@gmail.com>
2023-07-28 15:23:51 +0800

    Update index.js

--- api/1.0/chats/index.js
+++ api/1.0/chats/index.js
@@ -5,7 +5,7 @@ const { ErrorHandling, checkAuthorization, upload } = require('../utils/function
 const { createChat } = require('../controller/chats_controller');
 
 
-router.delete('/:user_id', checkAuthorization, (req, res) => {ErrorHandling(createChat(req, res), res)});
+router.post('/:user_id', checkAuthorization, (req, res) => {ErrorHandling(createChat(req, res), res)});
 
 
 module.exports = router;
