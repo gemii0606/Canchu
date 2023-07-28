8e5d45b59986814fbec1bde4a6f84a6b6ede3c13
gemii0606 <gemii0606@gmail.com>
2023-07-28 13:05:37 +0800

    Update index.js

--- api/1.0/groups/index.js
+++ api/1.0/groups/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { ErrorHandling, checkAuthorization, upload } = require('../utils/function');
-const { createGroup, groupDelete, groupJoin  } = require('../controller/groups_controller');
+const { createGroup, groupDelete, groupJoin, groupPending } = require('../controller/groups_controller');
 
 
 router.delete('/:group_id', checkAuthorization, (req, res) => {ErrorHandling(groupDelete(req, res), res)});
 router.post('/:group_id/join', checkAuthorization, (req, res) => {ErrorHandling(groupJoin(req, res), res)});
-router.get('/:group_id/member/pending', checkAuthorization, (req, res) => {ErrorHandling(userProfile(req, res), res)});
+router.get('/:group_id/member/pending', checkAuthorization, (req, res) => {ErrorHandling(groupPending(req, res), res)});
 // router.put('/picture', checkAuthorization, upload.single('picture'), (req, res) => {ErrorHandling(userPictureUpdate(req, res), res)});
 // router.get('/search', checkAuthorization, (req, res) => {ErrorHandling(userSearch(req, res), res)});
 router.post('/', checkAuthorization, (req, res) => {ErrorHandling(createGroup(req, res), res)});
