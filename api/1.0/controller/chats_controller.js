47c04fa0fe8c94bc641cdc22895f9f70d9b95d99
gemii0606 <gemii0606@gmail.com>
2023-07-28 15:21:49 +0800

    Update chats_controller.js

--- api/1.0/controller/chats_controller.js
+++ api/1.0/controller/chats_controller.js
@@ -3,23 +3,33 @@ const { Op } = require('sequelize');
 const moment = require('moment');
 
 const createChat = async (req, res) => {
+    const receiver_id = parseInt(req.params.user_id); 
     const decodedToken = req.decodedToken;
-    const user_id = decodedToken.id;
+    const sender_id = decodedToken.id;
 
-    const {name} = req.body;
+    const {message} = req.body;
 
-    if (!name) {
-        return res.status(400).json({error: 'group name should not be empty!'})
+    if (!message) {
+        return res.status(400).json({error: 'Message should not be empty!'})
     }
 
-    const group = await Group.create({
-        name,
-        leader_id: user_id
+    const find_user = await User.findOne({
+        where: {id: receiver_id}
+    })
+
+    if (!find_user) {
+        return res.status(400).json({error: 'User not found!'})
+    }
+
+    const chat = await Chat.create({
+        sender_id,
+        receiver_id,
+        message
       });
 
     const data = {
-        group: {
-            id: group.id
+        message: {
+            id: chat.id
         }
     }
     return res.status(200).json({data});
