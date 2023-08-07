0eb30c38072cdb2cbb5b2f91bda20802ad34ce3c
gemii0606 <gemii0606@gmail.com>
2023-08-08 00:48:23 +0800

    1

--- api/1.0/utils/redis.js
+++ api/1.0/utils/redis.js
@@ -1,13 +1,9 @@
 require('dotenv').config();
 const Redis = require('ioredis');
-let redisClient = null;
 
-// 根據需要初始化 Redis Client
-if (process.env.USE_REDIS === 'true') {
-  redisClient = new Redis({
+const redisClient = new Redis({
     port: 6379,
     host: process.env.RD_HOST
-  });
-}
+});
 
 module.exports = redisClient;
