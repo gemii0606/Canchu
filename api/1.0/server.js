668cc3aa079f6be31c0a3ac0cc4a5b0189a0d3e1
gemii0606 <gemii0606@gmail.com>
2023-08-07 11:28:16 +0800

    Update server.js

--- api/1.0/server.js
+++ api/1.0/server.js
@@ -18,7 +18,7 @@ const chatsRoute = require('./chats');
 
 // app.use(limiter);
 
-app.use(rateLimiter);
+// app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
