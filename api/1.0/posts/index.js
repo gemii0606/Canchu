3d37f090661e9990548e65985a08289855009e70
gemii0606 <gemii0606@gmail.com>
2023-08-09 15:03:50 +0800

    Update index.js

--- api/1.0/posts/index.js
+++ api/1.0/posts/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
+
 const { ErrorHandling, checkAuthorization } = require('../utils/function');
 const { postCreate, postComment, postLike, postLikeDelete, postSearch, postUpdate, postDetail } = require('../controller/posts_controller')
 
@@ -11,14 +11,5 @@ router.delete('/:id/like', checkAuthorization, async (req, res) => {ErrorHandlin
 router.get('/:id', checkAuthorization, async (req, res) => {ErrorHandling(postDetail(req, res), res)});
 router.put('/:id', checkAuthorization, async (req, res) => {ErrorHandling(postUpdate(req, res), res)});
 router.post('/', checkAuthorization, async (req, res) => {ErrorHandling(postCreate(req, res), res)});
-
-router.post('/test5000', async (req, res) => {
-    for (let i = 0; i <= 5000; i++) {
-        const requestData = JSON.stringify({context: i});
-        const response = await axios.post('https://13.210.26.62/api/1.0/posts', requestData);
-        console.log(`POST request ${i} status: ${response.status}`);
-
-    }
-});
 
 module.exports = router;
